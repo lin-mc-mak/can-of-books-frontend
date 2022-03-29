de84c921cf5e735464e068c0e6d40f6e06a5c8fd
Michael Maker <maker.mdm@gmail.com>
2022-03-29 17:02:51 -0500

    Add login form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,11 +1,19 @@
-import { Component } from "react";
+import { Component, Form } from "react";
 
 class LoginForm extends Component {
 
   render() {
     /* TODO: create a simple login form that collects username and and email, and lets parent component know when form has been submitted */
     return (
-      <p>LoginForm Coming Soon</p>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+      </Form>
     );
   }
 };
