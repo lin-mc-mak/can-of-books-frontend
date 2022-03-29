12b9ca401069fe73d71a3cf1c61ba43c88131245
Michael Maker <maker.mdm@gmail.com>
2022-03-29 17:02:17 -0500

    Add BestBooks and Profile component routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import BestBooks from './BestBooks';
+import Profile from './Profile';
 
 class App extends React.Component {
 
@@ -15,37 +17,48 @@ class App extends React.Component {
     this.state = {
       user: null,
       books: [],
+    }
   }
-}
 
-loginHandler = (user) => {
-  this.setState({
-    user,
-  })
-}
+  loginHandler = (user) => {
+    this.setState({
+      user: null,
+    })
+  }
 
-logoutHandler = () => {
-  this.setState({
-    user: null,
-  })
-}
+  logoutHandler = () => {
+    this.setState({
+      user: null,
+    })
+  }
 
-render() {
-  return (
-    <>
-      <Router>
-        <Header user={this.state.user} onLogout={this.logoutHandler} />
-        <Switch>
-          <Route exact path="/">
-            {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-          </Route>
-          {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  )
-}
+
+
+  render() {
+
+    console.log('app state',this.state);
+    return (
+      <>
+        <Router>
+          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              <BestBooks />
+            </Route>
+
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route exact path='/profile'>
+              <Profile
+                books={this.state.books} />
+            </Route>
+
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
 }
 
 export default App;
