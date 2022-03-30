3c36ea1ec040fb69f65c6198055622435927aed0
Michael Maker <maker.mdm@gmail.com>
2022-03-30 13:02:17 -0500

    Update footer text

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
-      </Navbar>
+      <Container className="bottom-footer">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>lin-mc-mak &copy;</Navbar.Brand>
+        </Navbar>
+      </Container>
     )
   }
 }
