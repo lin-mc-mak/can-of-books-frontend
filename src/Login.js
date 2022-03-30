15f3b95b3f633e2958bbb7518938ff326dda4ea9
Michael Maker <maker.mdm@gmail.com>
2022-03-30 13:10:15 -0500

    Update login Card width

--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,7 @@ import './Login.css';
 class Login extends React.Component {
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '75%' }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
           <Card.Text>
