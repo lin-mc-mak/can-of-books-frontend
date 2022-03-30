ee4f4afbcd68a080a06abc84e991648b9f894990
Michael Maker <maker.mdm@gmail.com>
2022-03-30 13:02:53 -0500

    Delete console log

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -34,7 +34,7 @@ class LoginForm extends Component {
   }
 
   render() {
-    console.log('form state', this.state);
+    // console.log('form state', this.state);
     /* TODO: create a "simple" login form that collects username and and email, and lets parent component know when form has been submitted */
     return (
       <Form onSubmit={this.handleSubmit}>
