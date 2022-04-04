94ee05b0261195158048dbef316b71163452c4f9
Michael Maker <maker.mdm@gmail.com>
2022-04-03 22:16:58 -0500

    Change old login button name to save code

---
+++ src/LoginButton-old.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import LoginForm from './LoginButton';
+
+export default class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonClicked: null,
+    }
+  }
+
+  handleLoginButton = () => {
+    this.setState({
+      buttonClicked: true
+    })
+  }
+
+  render() {
+    if (this.state.buttonClicked) {
+      return <LoginForm
+        handleUsernameInput={this.props.handleUsernameInput}
+        handleEmailInput={this.props.handleEmailInput} 
+        />
+    } else {
+      return < Button onClick={this.handleLoginButton} > Login</Button >
+    }
+  }
+}
+
+// import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// const LoginButton = () => {
+//   const { loginWithRedirect } = useAuth0();
+
+//   return <button onClick={() => loginWithRedirect()}>Log In</button>;
+// };
+
+// export default LoginButton;
